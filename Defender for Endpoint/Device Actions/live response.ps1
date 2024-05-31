265264548c1f10090b2aff1afc4a0e391a55dc2c
Abhishek Pratap <156197198+AbhishekPratap9@users.noreply.github.com>
2024-05-31 22:27:16 +0530

    Create live response.ps1

---
+++ Defender for Endpoint/Device Actions/live response.ps1
@@ -0,0 +1,12 @@
+#Download file with all prerequisites
+getfile c:\Users\user\Desktop\work.txt -auto
+#help
+help <command name>
+<command name> param1 param2
+<command name> -type file -id <file path> - auto or remediate file <file path> - auto
+# Analyze the file malware.txt
+
+analyze file c:\Users\user\Desktop\malware.txt
+# Analyze the process by PID
+
+analyze process 1234
